1672f02b82425fb035cef34ff3e77977a20e9242
Ben Altman <altmanguitar@gmail.com>
2021-01-19 04:18:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
     @total = 0
     @discount = discount
     @items = []
-    @last_item = last_item
+    @last_item = @items[@items.count - 1]
   end
 
   def add_item(item, cost, number = 1)
@@ -34,7 +34,7 @@ class CashRegister
   end
 
   def void_last_transaction
-    last_item = @items[@items.count - 1]
+    
   end
 
 end
